c1923a876f3592e62478df1d9a2f4390b057930f
Denisse Orellana <denissep.orellana@gmail.com>
2023-09-02 00:24:20 -0400

    add: helpers to set clases to the status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def status_color(instance)
+    if instance.status == 'ok'
+      class_name = 'badge bg-success'
+    elsif instance.status == 'warning'
+      class_name = 'badge bg-warning'
+    elsif instance.status == 'error'
+      class_name = 'badge bg-danger'
+    end
+
+    return raw("#{class_name}")
+  end
 end
