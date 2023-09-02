55ec3a19bd79cb6e74ace760bff10429a32d2c35
Denisse Orellana <denissep.orellana@gmail.com>
2023-09-02 00:09:01 -0400

    add: associations for the models

--- app/models/device_server.rb
+++ app/models/device_server.rb
@@ -1,3 +1,6 @@
 class DeviceServer < ApplicationRecord
   belongs_to :restaurant
+
+  has_one :server, dependent: :destroy
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many :device_servers, dependent: :destroy
+  accepts_nested_attributes_for :device_servers, reject_if: :all_blank, allow_destroy: true
+
+  has_many :main_server, dependent: :destroy
+  has_one :server, dependent: :destroy
 end
