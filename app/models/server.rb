2d1e60fa91c6e63d00ff75adc16015249d981ba8
Denisse Orellana <denissep.orellana@gmail.com>
2023-09-02 00:30:01 -0400

    add: method to update status servers

--- app/models/server.rb
+++ app/models/server.rb
@@ -12,4 +12,27 @@ class Server < ApplicationRecord
     warning:  1,
     error:    2
   }, _prefix: :restaurant_status
+
+  def is_updated?(server, device)
+    server.server_update != device.updated_at
+  end
+
+  def self.update_server_statuses
+    @devices = DeviceServer.all
+    @devices.each do |device|
+      @server = Server.new(
+        server_name:        device.name,
+        server_status:      device.status,
+        server_update:      device.updated_at,
+        device_server_id:   device.id,
+        restaurant_name:    device.restaurant.name,
+        restaurant_status:  device.restaurant.status,
+        restaurant_update:  device.restaurant.updated_at,
+        restaurant_id:      device.restaurant.id
+      )
+      @server.save
+    end
+
+    puts "####### Total servers updated: #{@devices.size} ######"
+  end
 end
