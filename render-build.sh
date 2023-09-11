224d0520a7ab87c457efc5be966ab0f484aebae2
Denisse Orellana <denissep.orellana@gmail.com>
2023-09-11 17:51:49 -0300

    change: render build

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,7 @@
 # exit on error
 set -o errexit
 
+bundle config set force_ruby_platform true # https://stackoverflow.com/a/66311533
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
